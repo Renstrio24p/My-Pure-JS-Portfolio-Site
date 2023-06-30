251d5547ced5eb800ad98473c5b25bccd1b3ad10
Renstrio24p <Renstrio24p@users.noreply.github.com>
2023-06-30 08:03:00 +0000

    deploy: b9d494527a3369f1a369e207d16b39498c1e1816

--- src/components/Home/Services.js
+++ src/components/Home/Services.js
@@ -6,19 +6,44 @@ export default function ServiceList(Services){
             <h1 class=${style_main('service-title')}>My Services</h1>
             <ul class=${style_main('servicelist')}>
                 <li class=${style_main('service-item')}>
-                        <h2>Web Development  <i class="fa-solid fa-code"></i></h3>
+                        <div class=${style_main('service-title')}>
+                            <h2>Web Development</h2>
+                            <div class=${style_main('service-icon')}>
+                                <i class="fa-solid fa-code"></i>
+                            </div>
+                        </div>
                         <p> Front-End Designing for UI/UX. 
                             SEO Optimization, Performance.</p>
                 </li>
                 <li class=${style_main('service-item')}>
-                        <h2>Data Analytics <i class="fa-solid fa-chart-simple"></i></h3>
+                        <div class=${style_main('service-title')}>
+                            <h2>Data Analytics</h2>
+                            <div class=${style_main('service-icon')}>
+                            <i class="fa-solid fa-chart-simple"></i>
+                            </div>
+                        </div>
                         <p> Clean and Optimized Data Errors from the
                             Database recontruct corrupted data.</p>
                 </li>
                 <li class=${style_main('service-item')}>
-                        <h2>Networking <i class="fa-solid fa-network-wired"></i></h3>
-                        <p> Front-End Designing for UI/UX. 
-                            SEO Optimization, Performance.</p>
+                    <div class=${style_main('service-title')}>
+                        <h2>Networking</h2>
+                        <div class=${style_main('service-icon')}>
+                            <i class="fa-solid fa-network-wired"></i>
+                        </div>
+                    </div>
+                        <p> Diagnositing Incoming Request from the server-side
+                             to protect unwanted request.</p>
+                </li>
+                <li class=${style_main('service-item')}>
+                    <div class=${style_main('service-title')}>
+                        <h2>Computer Setup</h2>
+                        <div class=${style_main('service-icon')}>
+                            <i class="fa-solid fa-computer"></i>
+                        </div>
+                    </div>
+                        <p> Setup computer units and optimize computer performance,
+                            Diagnosting Computer Errors.</p>
                 </li>
             </ul>
         `
