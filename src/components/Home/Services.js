d7cd6069b15b050be6e05d2e66123e8da11da9c9
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-06-30 12:45:16 +0800

    add SEO

--- src/components/Home/Services.js
+++ src/components/Home/Services.js
@@ -6,17 +6,17 @@ export default function ServiceList(Services){
             <h1 class=${style_main('service-title')}>My Services</h1>
             <ul class=${style_main('servicelist')}>
                 <li class=${style_main('service-item')}>
-                        <h3>Web Development  <i class="fa-solid fa-code"></i></h3>
+                        <h2>Web Development  <i class="fa-solid fa-code"></i></h3>
                         <p> Front-End Designing for UI/UX. 
                             SEO Optimization, Performance.</p>
                 </li>
                 <li class=${style_main('service-item')}>
-                        <h3>Data Analytics <i class="fa-solid fa-chart-simple"></i></h3>
+                        <h2>Data Analytics <i class="fa-solid fa-chart-simple"></i></h3>
                         <p> Clean and Optimized Data Errors from the
                             Database recontruct corrupted data.</p>
                 </li>
                 <li class=${style_main('service-item')}>
-                        <h3>Networking <i class="fa-solid fa-network-wired"></i></h3>
+                        <h2>Networking <i class="fa-solid fa-network-wired"></i></h3>
                         <p> Front-End Designing for UI/UX. 
                             SEO Optimization, Performance.</p>
                 </li>
