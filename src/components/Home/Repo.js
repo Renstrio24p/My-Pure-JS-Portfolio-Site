a151cd033ebe26101e9210bd86dc2b1a319c2e90
Renstrio24p <Renstrio24p@users.noreply.github.com>
2023-07-02 19:02:53 +0000

    deploy: 7f92929aa10e528bd049bf854ff4ea8e4f5a06f1

--- src/components/Home/Repo.js
+++ src/components/Home/Repo.js
@@ -1,30 +1,37 @@
 import { style_main } from "../../start";
+import Count from "./functions/count";
 
 export default function RepoList(Repo){
 
     Repo.innerHTML = (
         `
-            <div class=${style_main('repo-container')}>
-                <ul class=${style_main('repo-list')}>
-                    <li> 
-                        <h2>59</h2>
-                        <span>Github Repositories</span>
-                    </li>
-                    <li> 
-                        <h2>9</h2>
-                        <span>React Projects</span>
-                    </li>
-                    <li> 
-                        <h2>7</h2>
-                        <span>Javascript Projects</span>
-                    </li>
-                    <li> 
-                        <h2>2</h2>
-                        <span>Collaboration Projects</span>
-                    </li>
-                </ul>
-            </div>
+        <div class=${style_main('repo-container')}>
+            <ul class=${style_main('repo-list')}>
+                <li> 
+                    <h2 id="repo-count">59</h2>
+                    <span>Github Repositories</span>
+                </li>
+                <li> 
+                    <h2 id="react-count">9</h2>
+                    <span>React Projects</span>
+                </li>
+                <li> 
+                    <h2 id="js-count">7</h2>
+                    <span>Javascript Projects</span>
+                </li>
+                <li> 
+                    <h2 id="ts-count">7</h2>
+                    <span>Typescript Projects</span>
+                </li>
+                <li> 
+                    <h2 id="collab-count">2</h2>
+                    <span>Collab Projects</span>
+                </li>
+            </ul>
+        </div>
         `
     )
+
+    Count();
 
 }
