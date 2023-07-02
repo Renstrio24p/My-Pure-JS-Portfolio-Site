c0699b7710f02da0bca79257f1f14d9761090bf8
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-07-03 03:11:31 +0800

    remove dist

--- dist/src/index.js
+++
@@ -1,7 +0,0 @@
-import Start from "./start";
-import './sass/index.scss';
-import UniqueHash from "./security/Hashes";
-
-const DOM = document.querySelector('#root');
-DOM.id = UniqueHash();
-Start(DOM); // Renders the Element ID
