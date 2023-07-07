6925bf516dfde00ffd9775a214dcd13153d1f282
Renstrio24p <Renstrio24p@users.noreply.github.com>
2023-07-07 15:55:28 +0000

    deploy: 2e86f8b18cb8d77e7e28bc0106cfbbe04138be56

--- src/components/Home/projects.js
+++ src/components/Home/projects.js
@@ -1,21 +1,58 @@
 import { style_main } from "../../start";
 
-export default function Project(Proj){
+export default function Project(Proj) {
 
-    Proj.innerHTML = (
-        `
-         <div class=${style_main('proj-container')}>
-            <h1>Rendered Project</h1>
-            <div class=${style_main('project-list')}>
-            <h2>Templates</h2>
-            <ul>
-                <li>
-                
-                </li>
-            </ul>
+  Proj.innerHTML = ( 
+    `
+    <div class=${style_main('proj-container')}>
+      <h1>Project</h1>
+      <div class=${style_main('project-list')}>
+        <h2>Templates</h2>
+        <p>
+            I made these template for my personal use in exploring and compling stacks that I can easily navigate
+            those errors in my projects.
+        </p>
+        <ul>
+          <li>
+            <div class=${style_main('template-item')}>
+              <img src='./src/images/st-javascript.webp' alt='js template'/>
+              <div class=${style_main('template-text')}>
+                <h2>Standalone Javascript</h2>
+                <p>It's a template for vanilla Javascript powered by. Webpack to used as a live compiler
+                   simulation of the page.</p>
+              </div>
             </div>
-         </div>
-        `
-    )
-
+          </li>
+          <li>
+            <div class=${style_main('template-item')}>
+              <img src='./src/images/st-typescript.webp' alt='ts template'/>
+              <div class=${style_main('template-text')}>
+                <h2>Standalone Typescript</h2>
+                <p>It's a template for vanilla Typescript however typescript ability to know specific types.</p>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class=${style_main('template-item')}>
+              <img src='./src/images/st-react.webp' alt='react template'/>
+              <div class=${style_main('template-text')}>
+                <h2>Standalone React</h2>
+                <p>It's a template for React and I make it simple and clear design. using webpack</p>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class=${style_main('template-item')}>
+              <img src='./src/images/st-react-typescript.webp' alt='react typescript template'/>
+              <div class=${style_main('template-text')}>
+                <h2>Standalone React Typescript</h2>
+                <p>It's a template for React but it has a typescript feature added using webpack</p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  `
+  )
 }
